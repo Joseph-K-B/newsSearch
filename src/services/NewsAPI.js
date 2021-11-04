6d273cb4f091706067d62ea4f9b2a61bbc27c979
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2021-11-03 18:01:13 -0700

    updated query for top headlines in us

--- src/services/NewsAPI.js
+++ src/services/NewsAPI.js
@@ -1,5 +1,5 @@
 export const fetchNews = async () => {
-    const res = await fetch(`https://newsapi.org/v2/everything?q=apple&apiKey=${process.env.REACT_APP_NEWS_API}`);
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_NEWS_API}`);
     const articles = await res.json();
     console.log('news', articles);
     return articles.articles;
